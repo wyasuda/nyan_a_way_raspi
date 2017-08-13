c5053fed105c08edb83f3af1ff5e306050a95164
wyasuda <wataru.robo@gmail.com>
2017-08-13 13:25:26 +0900

    Create watchdog.sh

---
+++ code/watchdog.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+while true
+do
+  sleep 60
+  box_mount_ok=`df | grep box | grep -v grep | wc -l`
+  if [ $box_mount_ok -eq 0 ]; then
+    mount /mnt/DAV/box
+  fi
+
+  nyan_a_way_alive=`ps -ef | grep nyan_a_way.py | grep -v grep | wc -l`
+  if [ $nyan_a_way_alive -eq 0 ]; then
+    /home/pi/nyan_a_way.py &
+  fi
+done
